fe7ed46f89f9699b02952c741687ebeeddf937b2
anthroprose <acorley@anthroprose.com>
2016-07-16 14:50:52 -0700

    Switching to Intel Edison

---
+++ compost.py
@@ -0,0 +1,27 @@
+import boto.sns
+import os
+import sys
+import mraa
+import time
+from pprint import pprint
+
+if __name__ == "__main__":
+
+    x = mraa.Aio(0)
+
+    compostmsg = 'Compost Bucket at Location #1 is Ready for Pickup'
+
+    conn = boto.sns.connect_to_region('us-west-2')
+
+    while 1:
+
+        tmp = float(x.read())
+
+        if tmp <= 200:
+            try:
+                pprint(compostmsg)
+                pub = conn.publish(topic=os.environ['TOPIC'], message=compostmsg)
+                time.sleep(30)
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+
